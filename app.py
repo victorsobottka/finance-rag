166e828caadbef596948ddc01c93e075a19037bb
victor.sobottka <victor.sobottka@bse.eu>
2026-05-05 21:34:53 +0200

    Deploy: HF

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-import os
 import gradio as gr
 from dotenv import load_dotenv
 load_dotenv("/home/sobottka/Documents/Projects/finance-rag/.env")
